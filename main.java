b2cfe56ca4e3333cde4da2586d712a6d59539258
PaulJame5 <43470494+PaulJame5@users.noreply.github.com>
2019-11-23 10:37:03 +0000

    Create main.java
    Simple for loop for printing out on the console line

---
+++ main.java
@@ -0,0 +1,27 @@
+class Main 
+{
+  public static void main(String[] args) 
+  {
+
+    //Code of Java for loop  
+    for(int i=1; i<=10; i++)
+    {  
+      for(int j = 1; j <= i; j++)
+      {
+        System.out.print(j); 
+      }  
+      // Create New Line
+      System.out.println("");
+    } // End for loop i++
+    
+    //Code of Java for loop  
+    for(int i=10; i>=1; i--)
+    {  
+      for(int j = 1; j <= i; j++)
+      {
+        System.out.print(j); 
+      }  
+      System.out.println("");
+    } // end for loop i--
+  } // end main(args)
+} // end class Main
